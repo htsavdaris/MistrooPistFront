474c91b46c437efb691b857e327d419a9aab46fb
Harry <hatsav@micro.com>
2021-12-17 03:15:53 +0200

    Edit
    Signed-off-by: Harry <hatsav@micro.com>

--- src/app/features/fysika2/fysikalist2/fysikalist2.component.ts
+++ src/app/features/fysika2/fysikalist2/fysikalist2.component.ts
@@ -6,7 +6,7 @@ import { Fysiko } from 'src/app/models/fysiko';
 import { AuthService } from 'src/app/services/auth.service';
 import { Observable } from "rxjs";
 
-enum recordState  {
+enum recordState {
   undefined,
   new,
   edit,
@@ -25,36 +25,70 @@ export class Fysikalist2Component implements OnInit {
   isLoggedIn!: Observable<boolean>;
   itemDialogShow: boolean = false;
   submitted: boolean = false;
-  rState : recordState = recordState.undefined;
+  rState: recordState = recordState.undefined;
   AMdisabled: boolean = true;
   valueA: boolean = false;
   valueB: boolean = false;
   valueC: boolean = false;
   valueD: boolean = false;
-  itemForm!: FormGroup ; 
+  itemForm!: FormGroup;
   selectedItem: Fysiko = new Fysiko();
   errormsg: Message[] = [];
 
-  constructor(private fb:FormBuilder,private fysikaService : FysikaService, public authService : AuthService) {
+  constructor(private fb: FormBuilder, private fysikaService: FysikaService, public authService: AuthService) {
     this.isLoggedIn = authService.isLoggedIn$();
   }
 
   ngOnInit(): void {
-    this.itemForm =  this.fb.group({
-      fldam :['', Validators.required ],
-      fldeponymo:['', [Validators.required, Validators.minLength(5)] ]
+    this.itemForm = this.fb.group({
+      fldam: ['', Validators.required],
+      fldeponymo: ['', Validators.required],
+      fldonoma: ['', Validators.required],
+      fldpatronymo: [''],
+      flddiefthinsi: [''],
+      fldnomos: [''],
+      fldemail: [''],
+      fldtilefono: [''],
+      fldcertification: [''],
+      fldeidikotita: [''],
+      flda: ['']
     });
 
     this.fysikaService.getFysika().subscribe(
       (data) => {
-        this.fysika = data;  
+        this.fysika = data;
         //console.log(data);
       }
 
     );
   }
+  onRowSelect(event: any) {
+    this.selectedItem = event.data;
+    //console.log (event.data.fldam);
+    //console.log (this.selectedItem);
+    this.itemForm.patchValue({
+      fldam: this.selectedItem.fldam,
+      fldeponymo: this.selectedItem.fldeponymo,
+      fldonoma: this.selectedItem.fldonoma,
+      fldpatronymo: this.selectedItem.fldpatronymo,
+      flddiefthinsi: this.selectedItem.flddiefthinsi,
+      fldnomos: this.selectedItem.fldnomos,
+      fldemail: this.selectedItem.fldemail,
+      fldtilefono: this.selectedItem.fldtilefono,
+      fldcertification: this.selectedItem.fldcertification,
+      fldeidikotita: this.selectedItem.fldeidikotita,
+      flda: this.selectedItem.flda
 
-  save () {
+    });
+  }
+
+  onRowUnselect(event: any) {
+    console.log(event.data.fldam);
+
+    //this.messageService.add({severity:'info', summary:'Product Unselected',  detail: event.data.name});
+  }
+
+  save() {
     return 0;
   }
 
